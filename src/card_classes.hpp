6a81d67f1e4e764780c85caaf96e8a0e78520b06
C <mostlyengineered1@gmail.com>
2021-10-25 16:07:05 -0400

    hand calculation added

--- src/card_classes.hpp
+++ src/card_classes.hpp
@@ -17,6 +17,7 @@ vector<char> ranks{ '2', '3', '4', '5', '6',
         '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
 
 
+
 //Rule of 3
 // destructor
 // copy constructor
@@ -33,9 +34,26 @@ class Card {
             _cardID = cardID;
             _suit = suit;
             _cRank = cRank;
+            calculateHardValue();
         };
 
         ~Card(){
+            // delete this;
+        };
+
+        void calculateHardValue(){
+            _hardValue  = ((int)(_cRank)) - 48;  // All numbers (besides 'T') will be 2-9
+        
+            if ((_hardValue==36) || (_hardValue==26) || (_hardValue==33) || (_hardValue==27)){
+                _hardValue = (int)10;
+            } else if (_hardValue==17){
+                _hardValue = (int)11;  // Each ace will be recalculated if there is a bust on hand calculation
+            }
+
+            if (_cRank == 'A'){
+                _isAce = true;
+            }
+
 
         };
 
@@ -48,7 +66,8 @@ class Card {
         char _suit;
         bool _faceUp = false;
         bool _discarded = false;
-
+        bool _isAce = false;
+        int _hardValue;
     private:
 
 
@@ -69,59 +88,88 @@ class Hand{
         }
     };
 
-    void calculateHandValue(){};
+    void calculateHandValue(){
+        int aceCount = 0;
+        _handValue = 0;
+
+        for (int c=0; c<_handCards.size(); c++){
+            _handValue += _handCards[c]->_hardValue;
+            if (_handCards[c]->_isAce){
+                aceCount += 1;
+            }
+        }
+            
+        if (_handValue > 21){            
+            for (int a=aceCount; a>0 ; a-- ){
+                _handValue -= 10;  //decrement for each ace in the hand
+                if (_handValue <= 21){ //check if decrement makes it below 21
+                    break;
+                }
+            }
+        }
+
+        if (_handValue > 21){            
+            _handValue = 22; // 22 will be standard bust value
+        }
+    };
 
     void moveCardToHand(unique_ptr<Card> card){
         _handCards.emplace_back(std::move(card));
-        
+        calculateHandValue();
     };
 
 
+    int _handValue; //total value of one deck is 380
+    bool _isBust;
     vector<unique_ptr<Card>> _handCards;
+
 };
 
 class  AllCards : public Hand {
-private:
+    private:
 
-public:
+    public:
 
-    AllCards(int nDecks){
-        for (int n=0;n < nDecks;++n){ 
-            for (auto const& s : suits){
-                for (auto const& r : ranks){ 
-                    unique_ptr<Card> card(new Card(s, r, _curID));                 
-                    card->printCard();
-                    dealCardToShoe(std::move(card));
-                    // card.get()->printCard();
-                    // cout << card->_suit;
-                    // cout << (card->_suit);
-                    _curID += 1;
+        AllCards(int nDecks){
+            for (int n=0;n < nDecks;++n){ 
+                for (auto const& s : suits){
+                    for (auto const& r : ranks){ 
+                        unique_ptr<Card> card(new Card(s, r, _curID));                 
+                        card->printCard();
+                        dealCardToShoe(std::move(card));
+                        // card.get()->printCard();
+                        // cout << card->_suit;
+                        // cout << (card->_suit);
+                        _curID += 1;
 
+                    }
                 }
-            }
-        }        
-    };
+            }        
+        };
 
-    void discardCard(unique_ptr<Card> card){
-        _discardPile.moveCardToHand(std::move(card));
-    };
+        void discardCard(unique_ptr<Card> card){
+            _discardPile.moveCardToHand(std::move(card));
+        };
 
-    void dealCardToShoe(unique_ptr<Card> card){
-        _shoe.moveCardToHand(std::move(card));
-    };
+        void dealCardToShoe(unique_ptr<Card> card){
+            _shoe.moveCardToHand(std::move(card));
+        };
 
-    // void dealCardFromShoeToHand(unique_ptr<Card> card, Hand hand){
-    //     hand.moveCardToHand(std::move(card));
-    // };
+        // void dealCardFromShoeToHand(unique_ptr<Card> card, Hand hand){
+        //     hand.moveCardToHand(std::move(card));
+        // };
 
-    void dealIndexCardFromShoeToHand(int i, Hand hand){
-        hand.moveCardToHand(std::move(_shoe._handCards[i]));
-        // _shoe._handCards.erase(i);  //need to resize so there are no holes
-    };
+        void dealIndexCardFromShoeToHand(int i, Hand hand){
+            hand.moveCardToHand(std::move(_shoe._handCards[i]));
 
-    long _curID=0;
-    Hand _shoe;
-    Hand _discardPile;
+            // std::move(_shoe._handCards).erase(i);  //need to resize so there are no holes
+            // (_shoe._handCards).erase(i);  //need to resize so there are no holes
+    
+        };
+
+        long _curID=0;
+        Hand _shoe;
+        Hand _discardPile;
 
 };
 
--- test/utest.cpp
+++ test/utest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <optional>
 #include <vector>
 #include "../src/card_classes.hpp"
@@ -12,6 +13,49 @@ TEST(CardInstantiation, MakeDecks){
     EXPECT_EQ(test_cards2._curID, 260);
     Hand testhand;
 }
+
+TEST(CardInstantiation, CheckCardValues){
+    Card two = Card('H', '2', 0);
+    Card five = Card('H', '5', 0);
+    Card ten = Card('H', 'T', 0);
+    Card jack = Card('H', 'J', 0);
+    Card queen = Card('H', 'Q', 0);
+    Card king = Card('H', 'K', 0);
+    Card ace = Card('H', 'A', 0);
+
+    EXPECT_EQ(two._hardValue, 2);
+    EXPECT_EQ(five._hardValue, 5);
+    EXPECT_EQ(ten._hardValue, 10);
+    EXPECT_EQ(jack._hardValue, 10);
+    EXPECT_EQ(queen._hardValue, 10);
+    EXPECT_EQ(king._hardValue, 10);
+    EXPECT_EQ(ace._hardValue, 11);
+
+}
+
+
+TEST(CardInstantiation, CheckHandValues){
+    AllCards test_cards = AllCards(2);
+    Hand hand1;
+    Hand hand2;
+
+    // test_cards._shoe._handCards[1]->printCard();
+    // test_cards._shoe._handCards[4]->printCard();
+    // test_cards._shoe._handCards[9]->printCard();
+
+    hand1.moveCardToHand(std::move(test_cards._shoe._handCards[1])); // 3
+    hand1.moveCardToHand(std::move(test_cards._shoe._handCards[4])); // 6
+    hand1.moveCardToHand(std::move(test_cards._shoe._handCards[9])); // J
+
+    EXPECT_EQ(hand1._handValue, 19);
+    
+    hand2.moveCardToHand(std::move(test_cards._shoe._handCards[11]));
+    hand2.moveCardToHand(std::move(test_cards._shoe._handCards[12])); //Blackjack
+    hand2.moveCardToHand(std::move(test_cards._shoe._handCards[25])); //2nd ace makes this 12
+
+    EXPECT_EQ(hand2._handValue, 12);
+}
+
 
 
 TEST(CardInstantiation, CardDeal){
@@ -25,9 +69,21 @@ TEST(CardInstantiation, CardDeal){
     // test_cards3.dealIndexCardFromShoeToHand(1, testhand);
     // hand.moveCardToHand(std::move(test_cards._shoe._handCards[2]));
     hand.moveCardToHand(std::move(test_cards._shoe._handCards[1]));
-    hand.moveCardToHand(std::move(test_cards._shoe._handCards[2]));
-    hand.printHand();
+    hand.moveCardToHand(std::move(test_cards._shoe._handCards[8]));
+    hand.moveCardToHand(std::move(test_cards._shoe._handCards[9]));
+    hand.moveCardToHand(std::move(test_cards._shoe._handCards[10]));
+    hand.moveCardToHand(std::move(test_cards._shoe._handCards[11]));
+    hand.moveCardToHand(std::move(test_cards._shoe._handCards[12]));
+    // cout << "test value is: " << test_cards._shoe._handCards[1];
+    // hand.printHand();
+    // cout << ((int)(hand._handCards[0].get()->_cRank)) - 48 << endl;
+    // cout << ((int)(hand._handCards[1].get()->_cRank)) - 48 << endl;
+    // cout << ((int)(hand._handCards[2].get()->_cRank)) - 48 << endl;
+    // cout << ((int)(hand._handCards[3].get()->_cRank)) - 48 << endl;
+    // cout << ((int)(hand._handCards[4].get()->_cRank)) - 48 << endl;
+    // cout << ((int)(hand._handCards[5].get()->_cRank)) - 48 << endl;
 
+    
     
 
     EXPECT_EQ(1, 1);
